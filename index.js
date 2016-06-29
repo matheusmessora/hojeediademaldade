c5eaca58c9120f6000cdb2e6aef122dc50599583
matheus.messora <matheus.messora@netshoes.com>
2016-06-29 19:45:16 -0300

    backgrounds diferentes

--- index.js
+++ index.js
@@ -15,22 +15,41 @@ var data = [
   {
     trackId: "INSTALL-BAIDU",
     title: "instalar o BAIDU!",
-    img: "http://antivirus.baidu.com/r/image/2014-08-25/22a52239c422616442d2c35540ff1361.jpg"
+    img: "http://antivirus.baidu.com/r/image/2014-08-25/22a52239c422616442d2c35540ff1361.jpg",
+    css: {
+      background: "303F9F",
+      background: "303F9F",
+      accent: "d32f2f",
+      accent_dark: "FDD835"
+    }
   },
   {
     trackId: "UBER-TAXI",
-    title: "chamar um 99taxis e um Uber ao mesmo tempo!",
-    img: "http://sorrisosdesucesso.com/wp-content/uploads/2015/12/Uber-v-Taxi.jpg"
+    title: "chamar taxi e Uber ao mesmo tempo!",
+    img: "http://sorrisosdesucesso.com/wp-content/uploads/2015/12/Uber-v-Taxi.jpg",
+    css: {
+      background: "212121",
+      accent: "F9A825",
+      accent_dark: "F57F17"
+    }
   },
   {
     trackId: "WHILE-TRUE",
-    title: "fazer WHILE true",
-    img: "http://customize.org/thumbnails/larger/96648.jpg"
+    title: "fazer WHILE true no código",
+    img: "http://customize.org/thumbnails/larger/96648.jpg",
+    css: {
+      background: "37474F",
+      accent: "00E676"
+    }
   },
   {
     trackId: "ABRIR-IE",
     title: "abrir o IE",
-    img: "http://blog.speedexam.net/wp-content/uploads/2015/05/ie_8_rc1.jpg"
+    img: "http://blog.speedexam.net/wp-content/uploads/2015/05/ie_8_rc1.jpg",
+    css: {
+      background: "37474F",
+      accent: "0288D1"
+    }
   }
 ];
 
