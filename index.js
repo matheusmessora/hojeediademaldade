6330e021846eea755595c9055325dbc9e00a560c
matheus.messora <matheus.messora@netshoes.com>
2016-06-29 20:21:12 -0300

    mesmo background

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ var data = [
     title: "instalar o BAIDU!",
     img: "http://antivirus.baidu.com/r/image/2014-08-25/22a52239c422616442d2c35540ff1361.jpg",
     css: {
-      background: "303F9F",
-      background: "303F9F",
+      background: "303030",
       accent: "d32f2f",
       accent_dark: "FDD835"
     }
@@ -28,7 +27,7 @@ var data = [
     title: "chamar taxi e Uber ao mesmo tempo!",
     img: "http://sorrisosdesucesso.com/wp-content/uploads/2015/12/Uber-v-Taxi.jpg",
     css: {
-      background: "212121",
+      background: "303030",
       accent: "F9A825",
       accent_dark: "F57F17"
     }
@@ -38,7 +37,7 @@ var data = [
     title: "fazer WHILE true no código",
     img: "http://customize.org/thumbnails/larger/96648.jpg",
     css: {
-      background: "37474F",
+      background: "303030",
       accent: "00E676"
     }
   },
@@ -47,7 +46,7 @@ var data = [
     title: "abrir o IE",
     img: "http://blog.speedexam.net/wp-content/uploads/2015/05/ie_8_rc1.jpg",
     css: {
-      background: "37474F",
+      background: "303030",
       accent: "0288D1"
     }
   }
@@ -62,7 +61,10 @@ function getRandomInt(min, max) {
 app.get('/', function (req, res) {
 
   var random = getRandomInt(0,data.length);
-  res.render('index', data[random]);
+  res.render('index', {
+    data: data[random],
+    online: getRandomInt(1, 230)
+  });
 });
 
 app.listen(3000, function () {
