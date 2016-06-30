7d4eaf3403c3b48668b392572bdc579bd01641fc
matheus.messora <matheus.messora@netshoes.com>
2016-06-29 21:50:22 -0300

    using S3 for images

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var data = [
   {
     trackId: "INSTALL-BAIDU",
     title: "instalar o BAIDU!",
-    img: "http://antivirus.baidu.com/r/image/2014-08-25/22a52239c422616442d2c35540ff1361.jpg",
+    img: "https://s3.amazonaws.com/elasticbeanstalk-us-east-1-141499087849/static/baidu.jpg",
     css: {
       background: "303030",
       accent: "d32f2f",
@@ -26,7 +26,7 @@ var data = [
   {
     trackId: "UBER-TAXI",
     title: "chamar taxi e Uber ao mesmo tempo!",
-    img: "http://sorrisosdesucesso.com/wp-content/uploads/2015/12/Uber-v-Taxi.jpg",
+    img: "https://s3.amazonaws.com/elasticbeanstalk-us-east-1-141499087849/static/uber.jpg",
     css: {
       background: "303030",
       accent: "F9A825",
@@ -36,7 +36,7 @@ var data = [
   {
     trackId: "WHILE-TRUE",
     title: "fazer WHILE true no código",
-    img: "http://customize.org/thumbnails/larger/96648.jpg",
+    img: "https://s3.amazonaws.com/elasticbeanstalk-us-east-1-141499087849/static/whiletrue.jpg",
     css: {
       background: "303030",
       accent: "00E676"
@@ -45,7 +45,7 @@ var data = [
   {
     trackId: "ABRIR-IE",
     title: "abrir o IE",
-    img: "http://blog.speedexam.net/wp-content/uploads/2015/05/ie_8_rc1.jpg",
+    img: "https://s3.amazonaws.com/elasticbeanstalk-us-east-1-141499087849/static/ie8.jpg",
     css: {
       background: "303030",
       accent: "0288D1"
